43f4cd2c7b9eb15b4e03df6bdde9defafd2bb483
Tim S <tsantos@stanford.edu>
2013-06-15 23:38:31 -0700

    new rails project

---
+++ edmSite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdmSite::Application.config.secret_token = 'eb0c96117c9a697881241796093549bfb92629f2189ecaf8d8d10be602f0a9b8c8b391a81b7d22401fa090f40e37c3b031ba5968393c056638089240cc30ce71'
